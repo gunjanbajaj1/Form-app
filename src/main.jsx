38357d230ebdb5a93b23d6351bb2919c62c355de
gunjanbajaj1 <gunjanbajaj106@gmail.com>
2023-09-26 09:09:21 +0530

    add

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,16 @@ import FormV1 from "./component/FormV1";
 import * as React from "react";
 import "./component/FormV1.css";
 import Parent from "./component/Parent";
+import SearchExample from "./component/useEffect";
 
 
 const rootElement = document.getElementById("root");
 ReactDOM.createRoot(rootElement).render(
   <>
-    <Parent />
+  <div style={{display:"none"}}>
+  <Parent />
+  </div>
+    
+    <SearchExample />
   </>
 );
